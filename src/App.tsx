ecef7751e9e94431bda460a5a98a2637bd191c35
Limarev Ivan <ivantaketo@gmail.com>
2024-06-01 13:59:07 +0600

    Add food icons in ItemCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
 import {useState} from 'react';
+import Hamburger from "./assets/icons/ic-hamburger.svg";
+import Cheeseburger from "./assets/icons/ic-cheeseburger.svg";
+import Fries from "./assets/icons/ic-fries.svg";
+import Coffee from "./assets/icons/ic-coffee.svg";
+import Tea from "./assets/icons/ic-tea.svg";
+import Cola from "./assets/icons/ic-cola.svg";
 
 interface Food {
   name: string;
   price: number;
+  image: string;
 }
 
 interface FoodState {
@@ -12,21 +19,21 @@ interface FoodState {
 }
 
 const FOODS: Food[] = [
-  {name: 'Hamburger', price: 80},
-  {name: 'Cheeseburger', price: 90},
-  {name: 'Fries', price: 90},
-  {name: 'Coffee', price: 70},
-  {name: 'Tea', price: 80},
-  {name: 'Cola', price: 40},
+  {name: 'Hamburger', price: 80, image: Hamburger},
+  {name: 'Cheeseburger', price: 90, image: Cheeseburger},
+  {name: 'Fries', price: 90, image: Fries},
+  {name: 'Coffee', price: 70, image: Coffee},
+  {name: 'Tea', price: 80, image: Tea},
+  {name: 'Cola', price: 40, image: Cola},
 ];
 
 
 const App = () => {
   const [food, setFood] = useState([
-    {name: 'Hamburger', count: 1},
-    {name: 'Cheeseburger', count: 1},
+    {name: 'Hamburger', count: 0},
+    {name: 'Cheeseburger', count: 0},
     {name: 'Fries', count: 0},
-    {name: 'Coffee', count: 7},
+    {name: 'Coffee', count: 0},
     {name: 'Tea', count: 0},
     {name: 'Cola', count: 0},
   ]);
@@ -73,25 +80,28 @@ const App = () => {
     });
   };
 
+  const orderList = (
+    food.map((item, index) => (
+      item.count > 0 ? (
+        <div className="OrderItem" key={index + item.count + item.name}>
+          <span>{item.name}</span>
+          <strong> x{item.count}</strong>
+          <span> {getPrice(item)} KGS</span>
+          <button onClick={() => removeFoodItem(item.name)}>remove</button>
+        </div>
+      ) : null
+    )).filter(item => item !== null)
+  )
+
   return (
       <div className="App">
         <div className="OrderList col">
-          {food.map((item, index) => (
-            item.count > 0 ? (
-              <div className="OrderItem" key={index + item.count + item.name}>
-                <span>{item.name}</span>
-                <strong> x{item.count}</strong>
-                <span> {getPrice(item)} KGS</span>
-                <button onClick={() => removeFoodItem(item.name)}>remove</button>
-              </div>
-            ) : null
-          ))}
+          {orderList.length > 0 ? orderList : <><h3>Order list empty now</h3><p>Add some item in list</p></>}
         </div>
         <div className="FoodItems col">
-          <h1>Food items</h1>
           {FOODS.map((item) => (
-            <div className="ItemCard" onClick={() => addFoodItem(item.name)}>
-              <img src="" alt={item.name}/>
+            <div key={item.name} className="ItemCard" onClick={() => addFoodItem(item.name)}>
+              <img className="item-image" src={item.image} alt={item.name}/>
               <p>
                 <span>{item.name}</span>
                 <strong>Price: {item.price} KGS</strong>
